6871debf989abee2f80ff76161e4c8ceb1ed7d3d
LFLPereira <lflpereira001@gmail.com>
2024-12-15 01:22:25 -0300

    Add files via upload

---
+++ estoque_epaminondas/Controllers/utils.py
@@ -0,0 +1,30 @@
+import locale
+import os
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+def inputplus(int_or_float,string_question):
+    inplus = -1
+    while inplus < 0:
+        inp = input(string_question)
+        try:
+            if int_or_float == True:
+                if float(inp) >= 0:
+                    inplus = float(inp)
+                    return inplus
+                else:
+                    print("Valor invalido!")
+            else:
+                if int(inp) >= 0:
+                    inplus = int(inp)
+                    return inplus
+                else:
+                    print("Valor invalido!")
+        except ValueError:
+            print("Valor invalido!")
+
+def exibir_vetor(vetor):
+    print("ID:", vetor[0],"- Descrição:", vetor[1],"- Quantidade:", vetor[2], "- Valor:", locale.currency(vetor[3], grouping=True))
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
---
+++ estoque_epaminondas/Models/Database.py
@@ -0,0 +1,108 @@
+import sqlite3
+
+class Database:
+    def __init__(self, bd):
+        self.__conn = sqlite3.connect(bd)
+        self.__cursor = self.__conn.cursor()
+
+    def create_table(self, table, columns):
+        key = ''
+        for i in columns:
+            key += f'{i} {columns[i]}, '
+
+        query = f'''
+            CREATE TABLE IF NOT EXISTS {table}(
+                {key[:-2]}
+            )
+        '''
+        
+        if self.__cursor.execute(query):
+            self.__conn.commit()
+            return True
+        return False
+    
+    def get_all(self, table):
+        self.__cursor.execute(f'''
+            SELECT * FROM {table}
+        ''')
+        return self.__cursor.fetchall()
+    
+    def get_value_id(self, table, id):
+        self.__cursor.execute(f'''
+            SELECT * FROM {table} WHERE ID = "{id}"
+        ''')
+
+        return self.__cursor.fetchall()
+    
+    def get_value_name(self, table, name):
+        self.__cursor.execute(f'''
+            SELECT * FROM {table} WHERE Nome = "{name}"
+        ''')
+
+        return self.__cursor.fetchall()
+
+    def get_value(self, table, condition):
+        
+        where = ''
+        for i in condition:
+            where += f'WHERE {condition[i]}' if i == 'where' else ''
+
+        query = f'''
+            SELECT * FROM {table} {where}
+        '''
+
+        self.__cursor.execute(query)
+        return self.__cursor.fetchall()
+
+    def insert_values(self, table, values):
+        key = ''
+        value = ''
+
+        for i in values:
+            key += f'{i}, '
+            value += f'"{values[i]}", '
+
+        query = f'''
+            INSERT INTO {table}({key[:-2]}) VALUES ({value[:-2]});
+        '''
+
+        if self.__cursor.execute(query):
+            self.__conn.commit()
+            return True
+        return False
+
+    def update_values(self, table, values, condition):
+        key = ''
+        where = ''
+
+        for i in values:
+            key += f'{i} = "{values[i]}", '
+
+        for i in condition:
+            where += f'WHERE {condition[i]}' if i == 'where' else ''
+
+        query = f'''
+            UPDATE {table} SET {key[:-2]} {where}
+        '''
+
+        print(query)
+
+
+        if self.__cursor.execute(query):
+            self.__conn.commit()
+            return True
+        return False
+    
+    def delete_values(self, table, condition):
+        where = ''
+        for i in condition:
+            where += f' WHERE {condition[i]}' if i == 'where' else ''
+
+        query = f'''
+            DELETE FROM {table} {where}
+        '''
+
+        if self.__cursor.execute(query):
+            self.__conn.commit()
+            return True
+        return False
